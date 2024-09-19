4bce0022e62c92c40c9eceb97cf31afff220ca61
Anshu Kumar <anshu.kumar2017@vitalum.ac.in>
2024-09-20 01:27:39 +0530

    Added onboard route

---
+++ src/app/onboard/page.tsx
@@ -0,0 +1,13 @@
+
+import { OnboardingForm } from '@/components/OnboardingForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <OnboardingForm/>
+    </div>
+  )
+}
+
+export default page
