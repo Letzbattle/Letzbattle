eab11cdd61d44391693eb2ee525303adf222e5ce
Anshu Kumar <anshu.kumar2017@vitalum.ac.in>
2024-09-20 01:21:30 +0530

    Update login ui component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,24 @@
-"use client"
-import { signIn } from 'next-auth/react'
-import React from 'react'
+"use client";
+import { signIn } from "next-auth/react";
+import React from "react";
 
 const Login = () => {
-    const login=(provider:'google')=>{
-        signIn(provider,{
-            callbackUrl:'/'
-        })
+  const login = (provider: "google") => {
+    signIn(provider, {
+      callbackUrl: "/onboard",
+    });
 
-    }
+  };
   return (
-
     <div className="bg-black h-screen flex justify-center items-center">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>login("google")}>Login with google</button>
-        
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+        onClick={() => login("google")}
+      >
+        Login with google
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
