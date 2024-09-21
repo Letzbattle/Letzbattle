64a04a637244bd46ef143d918e0c5f21afe02ba8
Rishabh Jha <rishabhjha0@gmail.com>
2024-09-21 21:12:27 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,33 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import LatestQuestions from "./components/LatestQuestions";
 import Footer from "./components/Footer";
+import { auth } from '../../auth';
+import { db } from '@/lib/db';
+import { getUserById } from '@/data/user';
+import { redirect } from "next/navigation";
 import Header from "./components/Header";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const user = await getUserById(session?.user.id || '');
+
+  if (user) {
+    if (!user.isOnboarded) {
+      // Redirect to the onboarding page
+      return redirect('/onboard');
+    }
+  } else {
+    return redirect('/login'); 
+  }
+
+  console.log({ user });
+  
   return (
     <div className="bg-black">
-      <Header/>
-      <HeroSection/>
-      {/* <LatestQuestions/> */}
-      <Footer/>
+      <Header />
+      <HeroSection />
+      {/* <LatestQuestions /> */}
+      <Footer />
     </div>
   );
 }
