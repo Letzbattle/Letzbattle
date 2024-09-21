20ce605a67a53b1e47b0f6ff83bf22694b97103f
Rishabh Jha <rishabhjha0@gmail.com>
2024-09-21 14:41:38 +0530

    Try fixing server

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,7 @@ export default async function middleware(req:any) {
   if (isLoggedIn) {
     try {
       const userResponse = await fetch(
-        `https://bitter-quokka-letzbattle-e9e73964.koyeb.app/api/user`,
+        `https://letzbattle-backend.onrender.com/api/user`,
         {
           headers: {
             Authorization: `Bearer ${token?.idToken}`,
