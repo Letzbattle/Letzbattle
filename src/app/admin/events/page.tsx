b75f66d8f7dc7f7b3881639e0080e0fc57f651e6
Anshu Kumar <anshu.kumar2017@vitalum.ac.in>
2024-10-04 14:47:36 +0530

    fix ui

--- src/app/admin/events/page.tsx
+++ src/app/admin/events/page.tsx
@@ -43,9 +43,19 @@ function AdminEvents() {
               className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:scale-105"
             >
               <div className="p-4">
-                <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+                <div className="flex justify-between">
+                <h2 className="text-2xl font-semibold text-gray-900 dark:text-white">
                   {event.name}
                 </h2>
+                <button
+                  className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-40 text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                  onClick={() => router.push(`/admin/events/${event.id}`)}
+                >
+                  Send Email &rarr;
+                </button>
+                </div>
+                
+
                 <p className="text-gray-600 dark:text-gray-300 mt-2">
                   Event Date:{" "}
                   <span className="font-medium">
@@ -61,11 +71,18 @@ function AdminEvents() {
               <div className="p-4 bg-gray-100 dark:bg-gray-700 flex justify-end">
                 <button
                   // variant="primary"
-                  className="text-sm font-medium px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition"
+                  className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
                   onClick={() => router.push(`/admin/events/${event.id}`)}
                 >
-                  See Details
+                  See Details &rarr;
                 </button>
+                {/* <button
+                  // variant="primary"
+                  className="text-sm font-medium px-4 py-2 text-white bg-green-600 rounded-md hover:bg-blue-700 transition"
+                  onClick={() => router.push(`/admin/events/${event.id}`)}
+                >
+                  Send Email
+                </button> */}
               </div>
             </div>
           ))}
