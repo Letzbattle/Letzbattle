1e413563fd1d43191079b85dd0b61925bd447f80
Rishabh Jha <rishabhjha0@gmail.com>
2024-10-19 22:46:39 +0530

    fix

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -6,30 +6,30 @@ export default async function HeroSection() {
     const questions = {
         documents: [
             {
-                title: "Battleground",
+                title: "cricket",
                 $id: "1",
                 attachmentId: "react-guide",
-                thumbnail:"battleground.jpeg"
+                thumbnail:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRku-G5mzCVk1bW7rE8s6ti6FYZQCKxMH15Ud6RU6cwDhqJ_1Vl9-e9jTSgu_9AYZZGMQ4&usqp=CAU"
             },
             {
-                title: "FreeFire",
+                title: "Kabbadi",
                 $id: "2",
                 attachmentId: "js-guide",
-                thumbnail:"freefire.jpeg"
+                thumbnail:"https://www.newsonair.gov.in/wp-content/uploads/2024/08/kabaddi-league.jpg"
 
             },
             {
-                title: "Valorant",
+                title: "Football",
                 $id: "3",
                 attachmentId: "css-guide",
-                thumbnail:"valorant.jpeg"
+                thumbnail:"https://cdn.britannica.com/31/183231-050-8D8EB720/Carli-Lloyd-penalty-goal-semifinal-match-Germany-2015.jpg"
 
             },
             {
-                title: "Call of duty",
+                title: "Arm Wrestling",
                 $id: "4",
                 attachmentId: "css-guide",
-                thumbnail:"cod.jpeg"
+                thumbnail:"https://designforceinc.com/wp-content/uploads/2016/03/world-armwrestling-league-branding-style-guide.jpg"
 
             },
         ],
