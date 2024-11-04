6016345e8afc05a87ff1676d9509082583481673
Rishabh Jha <rishabhjha0@gmail.com>
2024-11-04 13:41:36 +0530

    Fix typo

--- src/app/components/AllEvents.tsx
+++ src/app/components/AllEvents.tsx
@@ -312,7 +312,7 @@ export function AllEvents() {
                       as="button"
                       className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                     >
-                      TotalTeam: {event.Participant.length}
+                      Total Team: {event.Participant.length}
                     </CardItem>
                     <CardItem
                       translateZ={20}
